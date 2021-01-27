78f02508bca864bcf27b2212e95202ed352a33e2
Denner Augusto De Paula Bernardes <denner.augusto90@gmail.com>
2021-01-27 16:06:55 -0300

    correcao do bug na transicao das imagens

--- javascript/banner.js
+++ javascript/banner.js
@@ -1,16 +1,14 @@
 function Changebg(){
     
     const images = [
-        'url(../images/desktop-image-hero-1.jpg)',
-        'url(../images/desktop-image-hero-2.jpg)',
-        'url(../images/desktop-image-hero-3.jpg)'
+        'url(./images/desktop-image-hero-1.jpg)',
+        'url(./images/desktop-image-hero-2.jpg)',
+        'url(./images/desktop-image-hero-3.jpg)'
     ]
 
     var fundo = document.getElementById('lado-a')
-    const bg = images[Math.floor(Math.random()* images.length)];
-    fundo.style.backgroundImage = bg
+    const bg = images [Math.floor(Math.random()* images.length)];
+    fundo.style ['background-image'] = bg
 }
 
 setInterval(Changebg, 1000)
-
-window.addEventListener('load',Changebg)
